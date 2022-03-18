ef9c836fa68f9d4333c61a861b349ce658bb5910
Daniel Andrews <daniel.korienko@gmail.com>
2022-03-18 16:33:34 +0200

    Popular and veggies components

---
+++ src/components/Category/index.jsx
@@ -0,0 +1,13 @@
+import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
+import {GiNoodles, GiChopSticks} from "react-icons/gi";
+import styled from "styled-components";
+
+const index = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default index;
--- src/components/Popular/index.jsx
+++ src/components/Popular/index.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 import axios from "axios";
+import styled from "styled-components";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 const Popular = () => {
   const [popular, setPopular] = useState([]);
@@ -9,6 +12,11 @@ const Popular = () => {
   }, []);
 
   const getPopular = async () => {
+    if (localStorage.getItem("popularRecipies")) {
+      setPopular(JSON.parse(localStorage.getItem("popularRecipies")));
+      return;
+    }
+
     const data = await axios
       .get(
         `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API}&number=9`
@@ -20,18 +28,80 @@ const Popular = () => {
         throw error;
       });
 
+    localStorage.setItem("popularRecipies", JSON.stringify(data));
+
     setPopular(data);
   };
 
   return (
-    <div>
-      {popular.map((recipe) => (
-        <div key={recipe.id}>
-          <p>{recipe.title}</p>
-        </div>
-      ))}
-    </div>
+    <Wrapper>
+      <h3>Popular Picks</h3>
+      <Splide
+        options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          gap: "5rem",
+        }}
+      >
+        {popular.map((recipe) => (
+          <SplideSlide key={recipe.id}>
+            <Card >
+              <p>{recipe.title}</p>
+              <img src={recipe.image} alt={recipe.title} />
+              <Gradient />
+            </Card>
+          </SplideSlide>
+        ))}
+      </Splide>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  margin: 4rem 0;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 
 export default Popular;
